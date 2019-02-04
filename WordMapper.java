8f1bbe479e56dfb5b5c641087fe806fcf5f57bc4
chaitubhure <chaitubhure@gmail.com>
2019-02-04 10:10:40 -0500

    Add files via upload

---
+++ WordMapper.java
@@ -0,0 +1,27 @@
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+ 
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+/**
+ * Created with IntelliJ IDEA.
+ * User: pascal
+ * Date: 16-07-13
+ * Time: 12:07
+ */
+public class WordMapper extends Mapper<Text,Text,Text,Text> {
+ 
+    private Text word = new Text();
+ 
+    public void map(Text key, Text value, Context context) throws IOException, InterruptedException
+    {
+        StringTokenizer itr = new StringTokenizer(value.toString(),",");
+        while (itr.hasMoreTokens())
+        {
+            word.set(itr.nextToken());
+            context.write(key, word);
+        }
+    }
+}
